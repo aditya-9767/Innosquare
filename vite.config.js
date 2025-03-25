d7e978fe1f2874bcd60fb6d8d4b83a3f9bde8e12
adityap64 <adityap@64-squares.com>
2025-03-25 14:45:09 +0530

    change in config file

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  
+  server: {
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: 5177, // Specify the port
+  },
 })
